78bff3e9c2a95068d303ef6ef6271e2cb13b9acd
TommyMoretti97 <tommasomoretti1997@gmail.com>
2023-12-14 15:04:54 +0100

    Javascript

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,25 +1,37 @@
 const url = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 
-const albums = [
- { "id": 384842207},
- { "id": 76311092},
- { "id": 508204251},
- { "id": 76311092},
- { "id": 721846},
- { "id": 721845},
- { "id": 721843},
- { "id": 721833},
- { "id": 1329897},
- { "id": 1329898},
- { "id": 1329889},
- { "id": 81763},
- { "id": 68346981},
- { "id": 664237},
- { "id": 70874562},
- { "id": 86773062},
- { "id": 71624},
- { "id": 345122517},
- { "id": 302204417},
- { "id": 6240279} 
-]
+const albumMixPreferiti = [384842207, 76311092, 508204251,721846,721845,721843 ];
 
+const albumRecenti =[721833,1329897,1329898,1329889,81763,68346981];
+
+const albumPiuAscoltati = [664237,70874562, 86773062, 71624, 345122517,302204417];
+
+const arrayMixPreferiti = [];
+
+const arrayRecenti = [];
+
+const arrayPiuAscoltati = [];
+
+albumMixPreferiti.forEach((el) => {
+    fetch(url + el)
+      .then((response) => response.json())
+      .then(data => arrayMixPreferiti.push(data));
+});
+
+console.log(arrayMixPreferiti);
+
+albumRecenti.forEach((el) => {
+    fetch(url + el)
+     .then((response) => response.json())
+     .then(data => arrayRecenti.push(data));
+});
+
+console.log(arrayRecenti);
+
+albumPiuAscoltati.forEach((el) => {
+    fetch(url + el)
+    .then((response) => response.json())
+    .then(data => arrayPiuAscoltati.push(data));
+});
+
+console.log(arrayPiuAscoltati);
