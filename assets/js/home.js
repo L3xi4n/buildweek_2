764e7af4a47d380036723226c9d74e4eb6a240a1
Carmelo Bonanno <melospone99@gmail.com>
2023-12-18 11:00:13 +0100

    ddd

--- assets/js/home.js
+++ assets/js/home.js
@@ -30,6 +30,8 @@ albumRecenti.forEach((id) => {
     });
 });
 
+
+
 albumPiuAscoltati.forEach((id) => {
   fetch(url + id)
     .then((response) => response.json())
@@ -74,4 +76,30 @@ function cardsBuongiorno(album) {
       </div>
     </div>
   </div>`;
+}
+
+function cardGrande(album) {
+  fetch(url + 1329897)
+  .then((response) => response.json())
+  .then((data) => {
+
+  const cardGrande = document.getElementById("cardGrande");
+  cardGrande.innerHTML += `<div class="row g-0">
+  <div class="col-md-2">
+    <img src="${album.cover}" class="img-fluid rounded-start mx-3 my-4" alt="..." >
+  </div>
+  <div class="col-md-10 text-white">
+    <div class="card-body">
+      <div class="display-2 fw-bold">${album.title}</div>
+      <div> ${album.artist.name}</div>
+      <div>Ascolta il nuovo singolo di ${album.artist.name}</div> 
+      <button type="button" class="btn btn-success text-dark border rounded-5 px-4 py-2 fw-bold" id="btnPlay">Play</button>
+      <button type="button" class="btn btn-dark border border-white rounded-5 px-4 py-2" id="btnSave">Salva</button>
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="18" fill="currentColor" id="icon" class="bi bi-three-dots" viewBox="0 0 16 16">
+        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+      </svg>
+    </div>
+  </div>
+</div>`;
+  })
 }
