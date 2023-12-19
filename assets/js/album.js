90e1efe1188100e96a07f98adc2be0fb9a25650a
L3xi4n <livianedelea@gmail.com>
2023-12-20 00:42:17 +0100

    migliorie searchbar e altre

--- assets/js/album.js
+++ assets/js/album.js
@@ -63,13 +63,53 @@ createAlbum()
 
 }
 
+function onSearch(event) {
+  event.preventDefault();
+
+  const query = event.target.elements.search.value;
+
+  const title = document.getElementById("staticBackdropLabel");
+  const container = document.getElementById("searchCardContainer");
+
+  container.innerHTML = `
+  <div class="container-fluid text-center">
+    <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>`;
+
+  fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
+    .then((response) => response.json())
+    .then(({ data }) => {
+      title.innerText = `Search for ${query}`;
+      container.innerHTML = "";
+
+      data?.forEach((item) => {
+        container.innerHTML += `
+          <div class="card mb-3" style="width: 49.6%">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${item.album.cover}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.artist.name}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      });
+    });
+}
 
 /* frecce avanti e indietro */
 // Aggiungi un gestore di eventi per le frecce
-document.getElementById('prevArrow').addEventListener('click', function() {
+document.getElementById("prevArrow").addEventListener("click", function () {
   window.history.back(); // Torna indietro nella cronologia del browser
 });
 
-document.getElementById('nextArrow').addEventListener('click', function() {
+document.getElementById("nextArrow").addEventListener("click", function () {
   window.history.forward(); // Avanti nella cronologia del browser
 });
